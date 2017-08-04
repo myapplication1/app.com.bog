251a48aee5923c3bee90743a8e36949ca86941f7
kris amoah <amoahkris@yahoo.com>
2017-08-04 14:36:45 -0700

    Sales and purchases added to contexgt

---
+++ app.com.bog/Data/_DbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;using app_com_bog.Models;
+
+namespace app_com_bog.Data
+{
+    public class _DbContext: DbContext
+    {
+    }
+}
+
+
+using System;using System.Collections.Generic;using System.Data.Entity;using System.Linq;using System.Web;using app_com_bog.Entities;using app_com_bog.Models;
+
+namespace app_com_bog.Data
+{
+
+    public class _DbContext : DbContext
+
+    {
+
+        public _DbContext()
+
+          : base("DefaultConnection")
+
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+
+
+        public DbSet<tb_sales_transactions> tb_sales_transactions { get; set; }
+        public DbSet<tb_purchases_transactions> tb_purchases_transactions { get; set; }
+
+
+
+    }
+}
